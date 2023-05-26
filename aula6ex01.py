e7fe7bbb8d03d3a3ca60d4019db4ede22f801377
jefferson <computadores.rp@gmail.com>
2023-05-26 19:47:38 -0300

    Começo das aulas.

---
+++ aula6ex01.py
@@ -0,0 +1,5 @@
+primeiroNumero = float(input('Digite um número: '))
+segundoNumero = float(input('Digite outro número: '))
+soma = primeiroNumero + segundoNumero
+
+print('A soma do número {} e do número {} vale: {}'.format(primeiroNumero,segundoNumero,soma))
