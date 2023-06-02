bd3324b4df4daa2b63b952a87338dd1bf6f3f75a
jefferson <computadores.rp@gmail.com>
2023-06-02 19:59:06 -0300

    Comentando código.

--- aula6a.py
+++ aula6a.py
@@ -1,5 +1,8 @@
+
+# variaveis
 primeiroNumero = float(input('Digite um número: '))
 segundoNumero = float(input('Digite outro número: '))
 soma = primeiroNumero + segundoNumero
 
+# corpo do programa
 print('A soma do número {} e do número {} vale: {}'.format(primeiroNumero,segundoNumero,soma))
