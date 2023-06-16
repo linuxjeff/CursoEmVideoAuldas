9674eb532058c192b57629990aebb5f5e8488290
jefferson <computadores.rp@gmail.com>
2023-06-16 12:44:48 -0300

    atualização

---
+++ aula07a.py
@@ -0,0 +1,13 @@
+# Variáveis
+n1 = int(input('Um valor: '))
+n2 = int(input('Outro valor: '))
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+di = n1 // n2
+e = n1 ** n2
+
+# Execução
+
+print('A soma é {}, \no produto é {} e a divisão é {:.2f}'.format(s, m, d), end=' ')
+print('Divisão inteira {} e potência {}'.format(di, e))
